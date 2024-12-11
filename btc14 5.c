a5b7df9607a5281c6ca3b2198de0825ffeecc270
Phanh2910 <phanphuocanh97@gmail.com>
2024-12-12 02:15:51 +0700

    Add files via upload

---
+++ btc14 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char word[100]="xin chao ten toi la Nguyen Manh Thang";
+	int length=strlen(word);
+	int count=0;
+	for(int i=0;i<length;i++){
+		if(word[i]==' '){
+			count++; 
+		} 
+	} 
+	 printf("chuoi tren co %d tu",count+1);
+	 return 0; 
+} 
