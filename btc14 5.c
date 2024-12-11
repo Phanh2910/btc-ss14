432b214bdcb004d99450dabebea0674660aad415
Phanh2910 <phanphuocanh97@gmail.com>
2024-12-12 02:18:12 +0700

    Update btc14 5.c

--- btc14 5.c
+++ btc14 5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char word[100]="xin chao ten toi la Nguyen Manh Thang";
+	char word[100]="xin chao ten toi la Phan Phuoc Anh";
 	int length=strlen(word);
 	int count=0;
 	for(int i=0;i<length;i++){
